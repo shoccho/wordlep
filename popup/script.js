2fab2dccfa4d26d842aba3b3dbddbc665ff56860
shoccho <sfb.shoccho@gmail.com>
2022-02-10 00:56:52 +0600

    updated db and fixed a bug

--- popup/script.js
+++ popup/script.js
@@ -1,7 +1,14 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { command: "run" }, function (response) {
-        if (response == undefined)document.getElementById("result").innerText = "This is not wordle!"
-        else document.getElementById("result").innerText = response
-
+        if (response == undefined) document.getElementById("header").innerText = "Either this is not wordle\n or\n Try please try reloading "
+        else {
+            document.getElementById("header").innerText = response.header
+            let ulElem = document.getElementById("list")
+            response.list.forEach(element => {
+                let tempElem = document.createElement("p")
+                tempElem.innerText = element
+                ulElem.appendChild(tempElem)
+            });
+        }
     })
 });
--- scripts/wordelp.js
+++ scripts/wordelp.js
@@ -8,51 +8,52 @@ let rows = board.childNodes
 let alreadyFiltered = []
 let leftChars = "[abcdefghijklmnopqrstuvwxyz]"
 let positionsFound = Array(5).fill(leftChars)
+let regex = null
 function bery_optimized_function() {
     rows.forEach(element => {
         if (element.getAttribute("letters").length == 5) {
+
             let tiles = element.shadowRoot.querySelectorAll("game-tile")
             tiles.forEach((tile, idx) => {
                 let char = tile.getAttribute("letter")
                 let eval = tile.getAttribute("evaluation")
-                let regex = null
+                alreadyFiltered.push(char)
                 if (eval == "correct") {
                     regex = found(idx, char)
+                    wordList = wordList.filter(word => word.match(regex))
 
                 } else if (eval == "present") {
                     regex = present(idx, char)
                     wordList = wordList.filter(word => word.includes(char))
                 }
-                else if (alreadyFiltered.includes(char)) return
+
+            });
+            tiles.forEach((tile, idx) => {
+                let char = tile.getAttribute("letter")
+                let eval = tile.getAttribute("evaluation")
+                if (alreadyFiltered.includes(char)) return
                 else if (eval == "absent") {
                     regex = absent(char)
-                    wordList = wordList.filter(word => !word.includes(char))
                 }
                 alreadyFiltered.push(char)
                 wordList = wordList.filter(word => word.match(regex))
             });
         }
     });
-    let output = ""
-    if (wordList.length < 30) {
-        output = "Remaining words"
-        wordList.forEach(el => {
-            output += "\n" + el
-        })
+    let output = {}
+    let header = ""
+    let list = []
+    if (wordList.length < 50) {
+        header = "Remaining words:"
+        list = wordList
     } else {
-        console.log("suggestion : ", wordList[0])
-        output += "\nTry a couple more guesses!\n words left = " + wordList.length
-        output += "\n\nRandom suggestions"
+        header += "Words left = " + wordList.length
+        header += "\nSuggestions"
+        list = wordList.sort(() => .5 - Math.random()).slice(0, 30)
 
-        let temp = wordList.sort(() => .5 - Math.random()).slice(0, 5)
-        temp.forEach(el => {
-            console.log(el)
-            output += "\n" + el
-        });
     }
-    return output
+    return { header, list }
 }
-
 function makeRegex() {
     let regexString = ""
     for (let i = 0; i < 5; i++) {
